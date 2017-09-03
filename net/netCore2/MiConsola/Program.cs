cf6f626f47384631cec7556a9a177541cbbd6c19
MatiasIac <matias.iacono@gmail.com>
2017-09-03 09:42:35 -0300

    First .Net Core 2.0 console app

---
+++ net/netCore2/MiConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
