0a128af38f98436ff9e6502e57ea5464e31b03ea
MatiasIac <matias.iacono@gmail.com>
2017-09-30 11:58:48 -0300

    Mas GO

--- GoLang/Basic/main.go
+++ GoLang/Basic/main.go
@@ -4,6 +4,16 @@ import (
 	"fmt"
 )
 
+func sumar(a, b int) int {
+	return a + b
+}
+
+func prueba() (string, string) {
+	return "hola", "mundo"
+}
+
 func main() {
-	fmt.Println("Hola Mundo!")
+	//fmt.Println(sumar(10, 20))
+	a, b := prueba()
+	fmt.Println(a, b)
 }
