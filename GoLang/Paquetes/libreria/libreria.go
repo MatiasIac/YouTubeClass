75166ef15b6cd2ac239c04614c9e1fcf519a97e6
MatiasIac <matias.iacono@gmail.com>
2017-10-22 12:13:03 -0300

    Segundo ejemplo

---
+++ GoLang/Paquetes/libreria/libreria.go
@@ -0,0 +1,9 @@
+package libreria
+
+func Sumar(a, b int) int {
+	return a + b
+}
+
+func Restar() {
+	
+}
---
+++ GoLang/Paquetes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"../Paquetes/libreria"
+)
+
+func main() {
+	libreria.Restar()
+	fmt.Println(libreria.Sumar(10, 30))
+}
