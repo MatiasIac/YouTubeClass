606ce19aa909a859e9bf555b6fd0013401d43a09
MatiasIacono <matias.iacono@gmail.com>
2018-09-26 20:50:36 +1200

    Injeccion de dependencias en .net core

---
+++ net/apiSample/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private MyService _myService;
+
+        public ValuesController(MyService myService) 
+        {
+            _myService = myService;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            
+            var resultado = new string[] { _myService.Message };
+            
+            _myService.Message = "hola";
+
+            return resultado;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ net/apiSample/Services/MyService.cs
@@ -0,0 +1,6 @@
+namespace apiSample {
+    public class MyService 
+    {
+        public string Message { get; set; }
+    }
+}
