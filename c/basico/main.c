0509a7596a2dc014fb37cc21ced4a9ebb5ab7bfe
Matias Iacono <matias.iacono@gmail.com>
2019-05-05 08:20:18 +1200

    Punteros en C

--- c/basico/main.c
+++ c/basico/main.c
@@ -6,8 +6,18 @@ int sumar(int a, int b) {
 }
 
 int main() {
-    int resultado = sumar(20, 30);
-    printf("Resultado: %d", resultado);
+    //int resultado = sumar(20, 30);
+    //printf("Resultado: %d", resultado);
+
+    int a = 30;
+    int *puntero;
+
+    puntero = &a;
+    
+    printf("valor de a: %d\n", a);
+    printf("direccion de a: %x\n", &a);
+    printf("direccion de puntero: %x\n", puntero);
+    printf("valor de puntero: %d\n", *puntero);
 
     return 0;
 }
