04ad21ceabdb4c81b0bcc2ba11649329117e6890
Matias Iacono <matias.iacono@gmail.com>
2019-08-15 22:19:40 +1200

    Principios SOLID: O

---
+++ net/solidPrinciples/O/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public interface IForma
+    {
+        void Dibujar();
+    }
+
+    public class Rectangulo : IForma
+    {
+        public void Dibujar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Circulo : IForma
+    {
+        public void Dibujar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Triangulo : IForma
+    {
+        public void Dibujar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Cuadrado : IForma
+    {
+        public void Dibujar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Dibujador
+    {
+        public void Dibujar(List<IForma> formas)
+        {
+            foreach (var forma in formas)
+            {
+                forma.Dibujar();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaDeFormas = new List<IForma>
+            {
+                new Circulo(),
+                new Rectangulo(),
+                new Rectangulo(),
+                new Triangulo(),
+                new Cuadrado()
+            };
+
+            new Dibujador()
+                .Dibujar(listaDeFormas);
+
+            Console.ReadLine();
+        }
+    }
+}
