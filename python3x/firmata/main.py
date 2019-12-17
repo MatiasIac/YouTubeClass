41a662af30ee2ce1da7cfcebd6b43d2271eadac3
Matias Iacono <matias.iacono@gmail.com>
2019-12-17 21:42:12 +1300

    pyfirmata

--- python3x/firmata/main.py
+++ python3x/firmata/main.py
@@ -1,9 +1,10 @@
 import pyfirmata2
 
 DELAY = 1
-PORT =  pyfirmata2.Arduino.AUTODETECT
+PORT = pyfirmata2.Arduino.AUTODETECT
 
 board = pyfirmata2.Arduino(PORT)
+
 board.samplingOn(100)
 board.analog[0].enable_reporting()
 board.analog[1].enable_reporting()
