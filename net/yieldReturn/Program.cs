585e28c3757433c41e3971fa8720087e74a7a23f
Matias Iacono <matias.iacono@gmail.com>
2020-03-28 17:30:06 +1300

    Added yield return sample

---
+++ net/yieldReturn/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace yieldReturn
+{
+    class Program
+    {
+        public static IEnumerable<int> GetLista(int start)
+        {
+            for (int i = start; i < i + 2;)
+            {
+                i += 2;
+                yield return i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var lista = GetLista(100).Take(100).ToList();
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
